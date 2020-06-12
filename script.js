6ff9708b70b8986a1eac53c42c5d3e70cfa51dfe
shaileshkamble <65939819+shaileshkamble@users.noreply.github.com>
2020-06-12 22:50:47 +0530

    Add files via upload
    file uploaded

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("signin").addEventListener("click",function(){
+    document.querySelector(".popups1").style.display="flex";
+})
+document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".popups1").style.display ="none";
+})
+
+document.getElementById("signup").addEventListener("click",function(){
+    document.querySelector(".popups2").style.display="flex";
+})
+document.querySelector(".close2").addEventListener("click",function(){
+    document.querySelector(".popups2").style.display ="none";
+})
+
+document.getElementById("developer").addEventListener("click",function(){
+    document.querySelector(".popups3").style.display="flex";
+})
+document.querySelector(".close3").addEventListener("click",function(){
+    document.querySelector(".popups3").style.display ="none";
+})
